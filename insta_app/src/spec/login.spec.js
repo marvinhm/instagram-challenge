082dbd18d2a218fe0ed172949909b774b69e4bc9
Marvin Matovu <marvinmatovu@gmail.com>
2019-09-24 13:04:11 +0100

    label for name input should exist

---
+++ insta_app/src/spec/login.spec.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Login from '../lib/login';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+describe('Login component', () => {
+    it('Display a welcome page', () => {
+        const wrapper = shallow(<Login />)
+        const text = wrapper.find('h1').text()
+
+        expect(text).toEqual("Login")
+    })
+
+    it('label for name input should exist', () => {
+        const wrapper = shallow(<Login />)
+        const label = wrapper.find('form label').text()
+
+        expect(label).toEqual("Name")
+    })
+
+    // it('name input should exist', () => {
+    //     const wrapper = shallow(<Login />)
+    //     const input = wrapper.find('form input');
+    //     const btn = wrapper.find
+    //     input.simulate('focus');
+    //     input.simulate('change', { target: { value: 'User1' } });
+    //     incrementBtn.simulate('click')
+
+    //     expect(label).toEqual("Name")
+    // })
+
+    // it('Starts with sign up page', () => {
+    //     const wrapper = shallow(<User />)
+    //     const 
+    // })
+})
